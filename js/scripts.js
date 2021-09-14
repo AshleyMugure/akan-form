7369b70cbdf86a67106139461195eff1c126b999
Ashley <ashley.mugure@student.moringaschool.com>
2021-09-15 00:13:32 +0300

    add function code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 function formSort() {
-  
+  let day = parseInt(document.getElementById("day").value);
+  let month = parseInt(document.getElementById("month").value);
+  let year = document.getElementById("year").value;
+  let gender = document.getElementById("gender").value;
+  let CC = parseInt(year.slice(0, 2));
+  let YY = parseInt(year.slice(2, 4));
+
+  let dayWeek = 
 }
